d0264f980f17cfb5cdcdc0e302dbabe6b44edcea
Denis <web@dgladkov.ru>
2021-05-18 10:51:35 +0300

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://#
  * Description: This is a Gutenberg block modal / popup plugin
  * Author: Gladkov Denis
- * Version: 1.1.0
+ * Version: 1.2.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
  * GitHub Plugin URI: https://github.com/z-website/modal-block_zw.git
